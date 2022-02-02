71c742206a0178500f54ac31ef034eebf0075722
ChandakaYugandhar <98861499+ChandakaYugandhar@users.noreply.github.com>
2022-02-02 12:03:02 +0530

    Update README.py

---
+++ README.py
@@ -0,0 +1,8 @@
+lottery="letsupgrade"
+print("select one letter from",lottery,"to win a lottery")
+input=input("enter the letter")
+if input==lottery[3] or input==lottery[6]:
+  print("conguratulations you win lottery")
+
+else:
+  print("sorry! try again.")
